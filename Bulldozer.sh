ac25d1b021fd18808b5b11360623be2c1abc56f3
Froakill <97559883+froaquiu@users.noreply.github.com>
2024-10-21 13:58:06 -0300

    Atualizar o Bulldozer.sh

--- Bulldozer.sh
+++ Bulldozer.sh
@@ -9,16 +9,115 @@ if ! command -v lolcat &> /dev/null; then
     exit 1
 fi
 
+if ! command -v figlet &> /dev/null; then
+    echo "Por favor, instale o figlet para executar este script."
+    exit 1
+fi
+
+function second_page {
+    clear
+    echo "Made by Assing" | lolcat
+    echo "O terror do 712" | lolcat
+    echo "Pressione qualquer tecla para voltar..."
+    read -n 1 -s
+}
+
+function check_vulnerabilities {
+    clear
+    read -p "Digite o URL do site (ex: example.com): " url
+    echo "Buscando vulnerabilidades em $url..."
+    
+    # Exemplo de comando com nmap (verifique se o nmap está instalado)
+    nmap -Pn --script=vuln "$url"
+
+    echo "Busca concluída. Pressione qualquer tecla para voltar..."
+    read -n 1 -s
+}
+
+function check_sql_vulnerabilities {
+    clear
+    read -p "Digite a URL do site (ex: http://example.com/page.php?id=1): " url
+    echo "Buscando vulnerabilidades SQL em $url..."
+    
+    # Comando com sqlmap
+    sqlmap -u "$url" --batch --level=5 --risk=3
+
+    echo "Busca concluída. Pressione qualquer tecla para voltar..."
+    read -n 1 -s
+}
+
+function display_art {
+
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠠⠾⠿⠯⢭⡉⠉⠙⠲⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠀⢣⠀⡌⢇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠔⠋⢉⣳⣦⣄⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⠁⣠⠊⢀⡇⡾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡏⠀⢰⠋⠀⠀⠉⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⡔⢡⠆⡴⠁⠀⢸⢠⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠀⠀⡜⡄⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡞⢠⠃⢰⡇⠀⠀⠀⢏⢷⣤⣀⣀⣠⣤⣄⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⢀⠀⢡⢹⡄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡇⠘⠀⣼⡇⠀⠀⠀⠈⢾⡇⠙⣄⠀⠀⠀⠀⠈⠉⠒⠦⣶⡒⠶⠶⠶⣄⣀⣼⠇⡌⠀⠘⡆⢿⡄⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢧⠀⡄⠸⣿⣆⠀⠀⠀⣼⠇⠀⣨⡃⠀⣀⣀⡀⠀⠀⠀⠀⠈⠓⠀⠀⠙⣿⠧⡾⠀⣴⢀⡇⢸⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠘⣆⠘⣆⠈⠻⣷⣤⣠⠟⠀⢀⣿⣿⠟⠁⠀⠱⡄⠀⡀⠀⠈⢳⣦⠀⠀⠘⢦⡘⣾⣿⣿⠇⢫⠇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣦⣼⣿⣶⠿⢟⣥⣤⣶⣾⡟⢁⣀⡀⠀⠀⢳⣄⣸⠳⡄⠀⠀⢣⠀⠀⢸⠏⠻⣿⡏⣠⠋⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣶⣤⣤⡤⠖⠋⠉⢉⣉⡉⠉⠉⠙⡿⠚⠟⢸⣭⣿⣽⣦⡀⠈⠁⢳⠀⢸⢀⣦⣎⣴⡁⢀⣠⣶⣾⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠈⠹⣿⡛⠿⣍⣉⠉⠉⠙⢦⣀⡎⠀⠀⠀⠈⠟⢻⣿⣿⣹⣦⣀⣠⡤⠖⠛⠛⠳⠄⡙⠻⢗⣻⠍⢑⡆⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠈⠙⢶⣤⣈⠉⠉⠁⠋⣿⣇⠀⠀⠀⠀⠀⠀⠉⠸⠛⣉⠍⠉⢳⠀⢀⣀⡀⠀⠀⠀⠀⢌⠳⢾⣇⣀⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣷⣶⠿⠋⢻⠷⣤⣀⠀⠀⠀⠀⠈⢉⡀⠀⠀⢸⣿⣵⠶⠾⣭⣙⠦⠀⠠⣧⠀⠉⠀⡽⠛⢳⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣴⢋⣿⡟⣿⢹⣿⠀⠀⠈⢣⡀⠙⣿⢷⣄⠀⣠⠔⠛⠑⠢⣽⠋⢠⠀⠀⠀⠈⠓⠦⣀⣸⣷⠤⠊⠀⠀⣼⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢀⣾⠃⠀⠹⠁⣿⠸⣿⠄⠀⠀⠀⠁⠀⠈⡃⢻⡞⣡⣶⣟⢢⡀⠸⡎⢺⣷⣄⡀⠀⠀⠀⠀⠈⠁⠀⠀⣄⣴⡏⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⣿⠃⠀⠀⠀⠀⣿⡀⠘⡄⠀⠀⠀⠀⠀⠀⢱⡞⢀⣿⡏⠈⠋⢿⢦⡈⠂⠙⣻⡿⢷⣄⠀⠀⠀⠀⡎⢀⡟⣿⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣾⡏⠀⠀⠀⠀⠀⠹⣷⣄⠀⠀⠀⢢⡀⠀⠀⠀⡇⢸⣿⢿⣴⠀⡀⠀⠿⠢⢤⡿⠃⣿⣍⣀⣀⣤⠾⠷⢻⣷⢸⡆⠀⠀⠀⠀
+⠀⠀⠀⢸⡿⠀⠀⠀⠀⠀⠀⠀⠘⢿⣷⣶⣤⣤⣉⡓⠒⢴⣇⠸⣿⡀⠉⠙⠧⣴⣀⣀⠀⢿⠀⢿⣖⠛⠛⠋⢙⣾⠏⡼⢸⠃⠀⠀⠀⠀
+⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⠛⠋⠛⠿⣾⣿⡄⠛⠷⣾⡥⠄⠒⠚⡛⠓⠚⢦⡈⠛⠷⠲⠿⠛⠁⠚⡠⠋⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⢦⡀⠀⠀⠈⢻⣿⣴⣾⣟⡛⠒⣒⠉⣀⣤⡶⣶⣿⠿⠷⣶⣶⠦⠴⠊⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠢⡀⠀⠀⠙⣿⣿⣽⣷⡀⠅⠉⠉⠀⠀⠀⠹⡄⠀⠈⢳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠤⠤⠶⣺⣿⣿⣿⣇⠀⠀⠀⠀⠀⢀⣠⠃⢀⡠⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣐⣶⣾⣿⠿⠟⠛⠉⠈⠑⠒⠶⠶⠾⠛⠛⠊⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" | lolcat
+
 # Título "BULLDOZER" com lolcat
-echo ">=>>=>    >=>     >=> >=>       >=>       >====>         >===>      >=======>>=> >=======> >======>     
->>   >=>  >=>     >=> >=>       >=>       >=>   >=>    >=>    >=>          >=>   >=>       >=>    >=>   
->>    >=> >=>     >=> >=>       >=>       >=>    >=> >=>        >=>       >=>    >=>       >=>    >=>   
->==>>=>   >=>     >=> >=>       >=>       >=>    >=> >=>        >=>     >=>      >=====>   >> >==>      
->>    >=> >=>     >=> >=>       >=>       >=>    >=> >=>        >=>    >=>       >=>       >=>  >=>     
->>     >> >=>     >=> >=>       >=>       >=>   >=>    >=>     >=>   >=>         >=>       >=>    >=>   
->===>>=>    >====>    >=======> >=======> >====>         >===>      >==========> >=======> >=>      >=> 
-                                                                                                        
-" | lolcat
+figlet "712 - BULLDOZER" | lolcat
+}
+
+display_art
+
+echo "===============================================" | lolcat
+
 
 # Mensagem adicional
-echo "Script executado com sucesso!" | lolcat
+echo -e "добро пожаловать в ад!" | lolcat
+
+echo -e "Выберите роль:"
+
+    echo "1. перезагрузить"
+    echo "2. узнать больше"
+    echo "3. искать уязвимости "
+    echo "4. уязвимости SQL"
+    echo "5. выйти"
+
+    read -p "введите свой вариант  (1/2/3/4): " option
+
+     case $option in
+        1)
+            display_art  # Reinicia a exibição da arte
+            ;;
+        2)
+            echo "abrindo" | lolcat
+            second_page
+            ;;
+        3)
+            check_vulnerabilities  # Busca vulnerabilidades
+            display_art  # Volta à exibição da arte após a busca
+            ;;
+        4)
+            echo "Fechando o script..."
+            exit 0  # Fecha o script
+            ;;
+        5)
+            echo "Fechando o script..."
+            exit 0  # Fecha o script
+            ;;
+        *)
+            echo "Opção inválida."
+            ;;
+    esac
+done
+
